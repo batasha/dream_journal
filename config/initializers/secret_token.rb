5d7d6294e3afd8a4085d0a863b1b57865fec1bf1
Natasha Badillo <natasha.badillo@gmail.com>
2013-05-24 09:00:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamJournal::Application.config.secret_token = '1442fbf50dab00f9a123553343291baaff17e851aabef5aaee0dc210000eb3ba75110608a5445318f0a26c78d2e219daa6120ec99d6bd986229f14536d392148'
